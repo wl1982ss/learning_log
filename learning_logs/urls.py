e1b06a7e4b64737f2f46f1e6efff1e1eb390d83c
wl1982ss <32494514@qq.com>
2018-11-14 19:28:34 +0800

    增加主题模板18.4.3

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""定义learning_logs的URL模式"""
+
+from django.conf.urls import url
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # 主页
+    url(r'^$', views.index, name='index'),
+
+    # 显示所有的主题
+    url(r'^topics/$', views.topics, name='topics'),
+]
