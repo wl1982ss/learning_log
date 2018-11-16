43daeca9ebc9e28d71ee55e3fd6267f5c25ef3e6
wl1982ss <32494514@qq.com>
2018-11-16 09:53:13 +0800

    增加显示特定主题的页面

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -6,7 +6,14 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 def topics(request):
-    """显示所有的主题"""
+    """显示所有主题"""
     topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """显示单个主题极其所有的条目"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries':entries}
+    return render(request, 'learning_logs/topic.html', context)
